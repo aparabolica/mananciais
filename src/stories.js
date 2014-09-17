5cefae2c31f1ef1a4b8089e239fdca205e81c680
Miguel Peixe <miguel@cardume.art.br>
2014-09-17 00:13:04 -0300

    clusterz!

--- src/stories.js
+++ src/stories.js
@@ -48,31 +48,39 @@ module.exports = function() {
 			stories.svg.x.scale.domain(domain.svg.x.domain());
 			stories.svg.y.scale.domain(domain.svg.y.domain());
 
+			var clusters = clusterize(stories.data);
+
 			stories.svg.node
 				.selectAll('line')
-				.data(stories.data)
+				.data(clusters)
 					.enter().append('line')
-					.attr("x1", stories.svg.x.map)
-					.attr("y1", stories.svg.y.map)
-					.attr("x2", stories.svg.x.map)
-					.attr("y2", stories.svg.y.offsetMap)
+					.attr("x1", function(d) { return d.cx; })
+					.attr("y1", function(d) { return d.cy; })
+					.attr("x2", function(d) { return d.cx; })
+					.attr("y2", function(d) { return d.cyO; })
 					.attr('class', 'story-line')
 					.style({stroke: '#fc0', 'stroke-width': '1px', 'stroke-opacity': .5});
 
 			stories.svg.node
 				.selectAll(".story")
-				.data(stories.data)
+				.data(clusters)
 					.enter().append("circle")
-					.attr("class", "story")
+					.attr("class", function(d) {
+						var c = 'story';
+						if(d.stories.length > 1) {
+							c += ' cluster';
+						}
+						return c;
+					})
 					.attr("r", 5)
-					.attr("cx", stories.svg.x.map)
-					.attr("cy", stories.svg.y.map)
+					.attr("cx", function(d) { return d.cx; })
+					.attr("cy", function(d) { return d.cy; })
 					.on("mouseover", function(d) {
 						stories.tooltip.transition()
 							.duration(200)
 							.style("opacity", 1);
 
-							stories.tooltip.html('<p class="date">' + d.date.format('DD/MM/YYYY') + '</p><h3>' + d.titulo + '</h3>')
+							stories.tooltip.html('<h3>' + d.titulo + '</h3>')
 							.style("left", (d3.event.pageX) + "px")
 							.style("top", (d3.event.pageY) + "px");
 					})
@@ -97,17 +105,28 @@ module.exports = function() {
 
 		stories.svg.x.scale.domain(extent);
 
-		stories.svg.node
-			.selectAll(".story")
-			.attr("cx", stories.svg.x.map)
-			.attr("cy", stories.svg.y.map);
+		var clusters = clusterize(stories.data);
 
 		stories.svg.node
-			.selectAll(".stories line")
-			.attr("x1", stories.svg.x.map)
-			.attr("y1", stories.svg.y.map)
-			.attr("x2", stories.svg.x.map)
-			.attr("y2", stories.svg.y.offsetMap);
+			.selectAll('line')
+			.data(clusters)
+			.attr("x1", function(d) { return d.cx; })
+			.attr("y1", function(d) { return d.cy; })
+			.attr("x2", function(d) { return d.cx; })
+			.attr("y2", function(d) { return d.cyO; });
+
+		stories.svg.node
+			.selectAll(".story")
+			.data(clusters)
+			.attr("class", function(d) {
+				var c = 'story';
+				if(d.stories.length > 1) {
+					c += ' cluster';
+				}
+				return c;
+			})
+			.attr("cx", function(d) { return d.cx; })
+			.attr("cy", function(d) { return d.cy; });
 
 	};
 
@@ -125,20 +144,74 @@ module.exports = function() {
 			}
 		};
 
-		stories.svg.node
-			.selectAll(".story")
-			.transition()
-			.duration(2000)
-			.attr("cy", stories.svg.y.map);
+		var clusters = clusterize(stories.data);
 
 		stories.svg.node
 			.selectAll('line')
+			.data(clusters)
 			.transition()
 			.duration(2000)
-			.attr("y1", stories.svg.y.map)
-			.attr("y2", stories.svg.y.offsetMap);
+			.attr("y1", function(d) { return d.cy; })
+			.attr("y2", function(d) { return d.cyO; });
+
+		stories.svg.node
+			.selectAll(".story")
+			.data(clusters)
+			.transition()
+			.duration(2000)
+			.attr("cy", function(d) { return d.cy; });
 
 	};
+
+	function clusterize(data) {
+
+		var sorted = _.sortBy(data, function(d) { return -stories.svg.x.scale(stories.svg.x.value(d)); });
+
+		var groups = [];
+
+		var prevVal;
+		var i = 0;
+		_.each(sorted, function(d) {
+			d._cx = stories.svg.x.scale(stories.svg.x.value(d));
+			d._cy = stories.svg.y.scale(stories.svg.y.value(d)) * 0.9;
+			d._cyO = stories.svg.y.scale(stories.svg.y.value(d));
+			if(prevVal - d._cx <= 10) {
+
+				if(!groups[i].length) {
+					groups[i] = [];
+				}
+
+				groups[i].push(d);
+			} else {
+				i++;
+				groups[i] = [d];
+			}
+			prevVal = d._cx;
+		});
+
+		groups = _.compact(groups);
+
+		var clusters = [];
+
+		_.each(groups, function(group) {
+
+			var cx = _.map(group, function(c) { return c._cx; }).reduce(function(prev, cur) { return prev + cur; }) / group.length;
+			var cy = _.map(group, function(c) { return c._cy; }).reduce(function(prev, cur) { return prev + cur; }) / group.length;
+			var cyO = _.map(group, function(c) { return c._cyO; }).reduce(function(prev, cur) { return prev + cur; }) / group.length;
+
+			clusters.push({
+				titulo: group.length + ' artigos',
+				stories: group,
+				cx: cx,
+				cy: cy,
+				cyO: cyO
+			});
+
+		});
+
+		return clusters;
+
+	}
 
 	return stories;
 
