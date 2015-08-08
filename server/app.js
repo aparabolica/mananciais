178bc54a78b0756b74780607a1f9394a82d94154
Miguel Peixe <miguel@cardume.art.br>
2015-08-08 19:29:38 -0300

    remove cli

--- server/app.js
+++ server/app.js
@@ -16,26 +16,8 @@ function print(s) {
 program
 	.version('0.0.1')
 	.option('-u, --update', 'Scrape data from SABESP and update local database')
-	.option('-d, --date [value]', 'Get specific date from database.')
-	.option('-m, --manancial [value]', 'Get info on specific date from an especific water system. Date parameter is required')
 	.option('serve', 'Run server and update database on a 3 hours interval')
 	.parse(process.argv);
-
-if(program.date) {
-	if(typeof(program.date) == 'string') {
-		csv().from.path('data/data.csv', {
-			columns: true
-		}).to.array(function(data) {
-			if(program.manancial && typeof(program.manancial) == 'string') {
-				print('\n{yellow}{bold}Buscando dados em: ' + program.date + ' de ' + program.manancial + '{/bold}{/yellow}\n');
-				print(tablify(_.filter(data, function(d) { return d['data'] == program.date && d['manancial'] == program.manancial; })));
-			} else {
-				print('\n{yellow}{bold}Buscando dados em: ' + program.date + '{/bold}{/yellow}\n');
-				print(tablify(_.filter(data, function(d) { return d['data'] == program.date; })));
-			}
-		});
-	}
-}
 
 if(program.update) {
 
